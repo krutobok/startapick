71ec052f2dbfa322a213648e2568906a168f1cb6
krutobok <55453895+krutobok@users.noreply.github.com>
2022-03-10 19:55:06 +0200

    0.1 alfa

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+
+    let $advantages;
+
+    $advantages = ($(window).width()-2326)/2;
+
+
+    $(document).ready(function() {
+        $('.advantages__bottom').css("margin-left", $advantages + "px");
+    });
+
+  
+});
