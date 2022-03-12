feb3d9853011956222512a46fcb74a5dd7f899b2
krutobok <55453895+krutobok@users.noreply.github.com>
2022-03-12 19:21:09 +0200

    0.3 alfa

--- js/main.js
+++ js/main.js
@@ -10,5 +10,18 @@ $(function(){
         $('.team-bg').css("left", $advantages + "px");
     });
 
+    $('.blog__items').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false
+    });
+
+    $('.blog__item').hover(function () {
+        $( ".blog__item-title", this ).addClass( "active" );
+    }, function () {
+        $( ".blog__item-title", this ).removeClass( "active" );
+    })
   
 });
